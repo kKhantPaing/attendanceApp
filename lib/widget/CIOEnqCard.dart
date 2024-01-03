47904b3b7f512b6885b8cfb3f95592e9304cc038
https://github.com/kKhantPaing <kkhantforjob@gmail.com>
2024-01-03 16:58:22 +0630

    update some

--- lib/widget/CIOEnqCard.dart
+++ lib/widget/CIOEnqCard.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:attendance_app/models/CheckInOutEnqModel.dart';
 
 class CIOEnqCard extends StatefulWidget {
   const CIOEnqCard({super.key});
